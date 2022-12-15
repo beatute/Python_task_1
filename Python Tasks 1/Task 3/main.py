0d03ca9861b5f4c8e4d80f59a7c832f77e94def1
beatute <110552465+beatute@users.noreply.github.com>
2022-12-15 17:56:45 +0200

    Add files via upload

---
+++ Python Tasks 1/Task 3/main.py
@@ -0,0 +1,18 @@
+# Turimas "audi" dict.
+
+# Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
+# ir grąžins visus jo "values" list'e.
+
+audi = {
+  "make": 'audi',
+  "model": 'A6',
+  "year": 2005,
+  "color": 'white',
+}
+
+
+def showObjectKeys(object):
+  values = list(object.values())
+  print (values)
+
+showObjectKeys(audi)
